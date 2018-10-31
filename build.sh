fcecf0de4cf7ad9199975a8bc8f76973a4506568
jingjie <jingjie520@gmail.com>
2018-10-31 15:35:03 +0800

    初始化beego项目

---
+++ build.sh
@@ -0,0 +1,2 @@
+export GOPATH=C:\\Work\\Project\\VSCORE\\GoShop
+go build -v -o bin/GoShop.exe src/main.go
---
+++ src/webApp/routers/router.go
@@ -0,0 +1,11 @@
+package routers
+
+import (
+	"webApp/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+}
